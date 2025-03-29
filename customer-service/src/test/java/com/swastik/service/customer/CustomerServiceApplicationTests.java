c05551e9a7af462d405d2aea9a41fd2829c23c06
gorla satyanarayana <gorla.satyanarayana.2708@gmail.com>
2025-03-29 21:31:04 +0700

    Initial commit for customer service

---
+++ customer-service/src/test/java/com/swastik/service/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swastik.service.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
